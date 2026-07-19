d9014a10733794084af179662c8159fc65851e9b
Derek Pitman <derek.j.pitman@gmail.com>
2026-07-19 14:22:41 -0400

    Lesson 01

--- main.c
+++ main.c
@@ -6,9 +6,23 @@
 
 int main(void) {
     char line[1024];
+    int state = 3;
     while (fgets(line, sizeof line, stdin)) {
+        char cmd[64];
         if (line[0] == '\n' || line[0] == 0) continue;
-        printf("TODO\n");
+        if (sscanf(line, "%63s", cmd) != 1) continue;
+        if (strcmp(cmd, "SYSCALL") == 0) {
+            state = 0;
+        }
+        else if (strcmp(cmd, "IRET") == 0) {
+            state = 3;
+        }
+        else if (strcmp(cmd, "INTR") == 0) {
+            state = 0;
+        }
+        else if (strcmp(cmd, "PROBE") == 0) {
+            printf("%d\n", state);
+        }
     }
     return 0;
 }
